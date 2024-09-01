05c7a3a1c53172286dc65c42f705abdf311c225b
piemontez <rafaelpiemontez@gmail.com>
2024-09-01 10:25:12 -0300

    refactor docker start

--- bin/docker-run-backend.sh
+++ bin/docker-run-backend.sh
@@ -14,12 +14,29 @@ mkdir -p ../backend/frontend
 yes | cp -rf ../frontend_web/build/* ../backend/frontend 
 
 # Troca para a pasta docs
-cd ../backend/docker
+cd ../backend
+
+# Carrega as variáveis de ambiente
+if test -f "docker/.env.docker"; then
+    source docker/.env.docker
+else
+    source docker/.env.docker.dev
+fi
+
+# Build o container
+echo "Buildando o container"
+docker build -t easylog_backend_server .
+
+# Para e remove o container atual se existir
+echo "Para e remove o container atual se existir"
+docker stop easylog_backend_server || true
+docker container rm easylog_backend_server || true
 
 # Inicia a aplicação
+echo "Iniciando na port ${SYSTEM_PORT}"
 
-if test -f ".env"; then
-    docker-compose --env-file .env up
-else
-    docker-compose --env-file .env.dev up
-fi
+docker run \
+    -v ".:/var/www/app" \
+    -p 3002:3002 \
+    --name easylog_backend_server \
+    easylog_backend_server
